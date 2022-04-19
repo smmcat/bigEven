371c602bc1e9cb1adeeea9cc9d5020254bba11f7
smmcat <1365116009@qq.com>
2022-04-19 09:19:21 +0800

    完成项目

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,7 +14,7 @@
 
 // 当每次发起 ajax 请求时 会先调用 $.ajaxPrefilter 函数
 // 该函数可 获得每次 ajax请求时 提供的配置对象
-$(function(){
+$(function () {
     $.ajaxPrefilter(function (options) {
         // 项目的请求根路径
         let url = 'http://www.liulongbin.top:3007';
